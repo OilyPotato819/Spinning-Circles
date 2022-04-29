248b86847372980ea5f7f083213b1a10cc7d9e6a
OilyPotato819 <o.schneider@share.epsb.ca>
2022-04-28 22:32:01 -0600

    made everything relative to size of canvas

--- script.js
+++ script.js
@@ -4,87 +4,96 @@ let ctx = cnv.getContext('2d');
 cnv.width = window.innerWidth - 25;
 cnv.height = window.innerHeight - 25;
 
+let relativeNum = getRelativeNum();
+
+function getRelativeNum() {
+   return Math.sqrt(cnv.width * cnv.height);
+}
+
 let dots = [];
 
 let mainDot = {
-   speed: 2,
+   speed: 0.003,
    x: cnv.width / 2,
    y: cnv.height / 2,
    directionX: 1,
    directionY: 1,
-   radius: 10,
+   radius: 0.02,
    wallsHit: 0,
    cornerHit: false,
    cornerCount: 0,
    currentColor: 0,
    colorCount: 0,
    draw: function () {
-      function randNum () {
+      this.relativeRadius = this.radius * relativeNum;
+      this.relativeSpeed = this.speed * relativeNum;
+
+      function randNum() {
          return Math.floor(Math.random() * 255);
       }
-      
+
       if (this.wallsHit > 0) {
          this.r = randNum();
          this.g = randNum();
          this.b = randNum();
-         
+
          this.color = `rgb(${this.r}, ${this.g}, ${this.b})`;
       }
-      
-      ctx.fillStyle = this.color || "black";
-      ctx.strokeStyle = this.color || "black";
+
+      ctx.fillStyle = this.color || 'black';
+      ctx.strokeStyle = this.color || 'black';
       ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI); 
-      
+      ctx.arc(this.x, this.y, this.relativeRadius, 0, 2 * Math.PI);
+
       if (this.cornerHit) {
          ctx.stroke();
          this.cornerCount++;
       } else {
          ctx.fill();
-         ctx.strokeStyle = `rgb(${this.r - 50}, ${this.g - 50}, ${this.b - 50})`;
+         ctx.strokeStyle = `rgb(${this.r - 50}, ${this.g - 50}, ${
+            this.b - 50
+         })`;
          ctx.beginPath();
-         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI); 
+         ctx.arc(this.x, this.y, this.relativeRadius, 0, 2 * Math.PI);
          ctx.stroke();
       }
-      
+
       this.wallsHit = 0;
 
-      if (this.x + this.radius >= cnv.width) {
+      if (this.x + this.relativeRadius >= cnv.width) {
          this.directionX = -1;
          this.wallsHit++;
-      } else if (this.x - this.radius <= 0) {
+      } else if (this.x - this.relativeRadius <= 0) {
          this.directionX = 1;
          this.wallsHit++;
       }
 
-      if (this.y + this.radius >= cnv.height) {
+      if (this.y + this.relativeRadius >= cnv.height) {
          this.directionY = -1;
          this.wallsHit++;
-      } else if (this.y - this.radius <= 0) {
+      } else if (this.y - this.relativeRadius <= 0) {
          this.directionY = 1;
          this.wallsHit++;
       }
-      
+
       if (this.wallsHit === 2) {
          this.cornerHit = true;
       }
-      
+
       if (this.cornerCount >= 50) {
          this.cornerCount = 0;
          this.cornerHit = false;
       }
 
-      this.x += this.directionX * this.speed;
-      this.y += this.directionY * this.speed;
+      this.x += this.directionX * this.relativeSpeed;
+      this.y += this.directionY * this.relativeSpeed;
    },
 };
-dots.push(mainDot);
 
 class orbiter {
    constructor(centralDot, distance, color, size) {
       this.centralDot = centralDot;
       this.distance = distance;
-      this.degreesNum = distance * 1.5;
       this.color = color;
       this.size = size;
       this.angle = 0;
@@ -92,33 +101,43 @@ class orbiter {
    }
 
    draw() {
+      this.relativeSize = (this.size || 0.01) * relativeNum;
+      this.relativeDistance = this.distance * relativeNum;
+
+      this.degreesNum = this.distance * 1200;
+
       this.x =
          this.centralDot.x +
-         this.distance * Math.cos((this.angle * Math.PI) / this.degreesNum);
+         this.relativeDistance *
+            Math.cos((this.angle * Math.PI) / this.degreesNum);
       this.y =
          this.centralDot.y +
-         this.distance * Math.sin((this.angle * Math.PI) / this.degreesNum);
+         this.relativeDistance *
+            Math.sin((this.angle * Math.PI) / this.degreesNum);
 
       this.angle++;
 
       ctx.fillStyle = this.color;
       ctx.beginPath();
-      ctx.arc(this.x, this.y, this.size || 5, 0, 2 * Math.PI);
+      ctx.arc(this.x, this.y, this.relativeSize, 0, 2 * Math.PI);
       ctx.fill();
    }
 }
 
-outsideDot1 = new orbiter(mainDot, 50, 'blue');
-outsideDot2 = new orbiter(mainDot, 30, 'green');
-outsideDot3 = new orbiter(mainDot, 70, 'red');
-outsideDot4 = new orbiter(outsideDot3, 15, 'yellow', 3);
-outsideDot5 = new orbiter(mainDot, 100, 'orange');
+outsideDot1 = new orbiter(mainDot, 0.045, 'blue');
+outsideDot2 = new orbiter(mainDot, 0.07, 'green');
+outsideDot3 = new orbiter(mainDot, 0.1, 'red');
+outsideDot4 = new orbiter(outsideDot3, 0.02, 'yellow', 0.006);
+outsideDot5 = new orbiter(mainDot, 0.12, 'orange');
+
+dots.push(mainDot);
 
 requestAnimationFrame(loop);
 
 function loop() {
    cnv.width = window.innerWidth - 25;
    cnv.height = window.innerHeight - 25;
+   relativeNum = getRelativeNum();
 
    ctx.fillStyle = 'white';
    ctx.fillRect(0, 0, cnv.width, cnv.height);
